25a4fa0cff737d091c1e1786dac80ee6731dc466
hxuhack <china.xuhui@gmail.com>
2016-11-30 13:26:28 +0800

    update

--- src/paraprog_fork.c
+++ src/paraprog_fork.c
@@ -1,22 +1,27 @@
 /*
 TOY:
 */
+#include <unistd.h> 
 #include "utils.h"
 
 int main(int argc, char** argv){
-    int pid;
-    int i=atoi(argv[1]);
-
+    int pid, fd[2];
+    pipe(fd);
     if((pid=fork())==-1)
 	return -1;
     if(pid == 0){
-        i++;
+        close(fd[0]);
+        write(fd[1],argv[1],sizeof(argv[1]));
+	wait(NULL);
     }
-    
     else{
-        if(i==2){
+	char content[8];
+        close(fd[1]);
+        read(fd[0],content,8);
+        if(strcmp(content,"7")==0){
             Bomb();
+        }else{
+            Foobar();
         }
-        Foobar();
     }
 }
