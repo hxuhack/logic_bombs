66e95567e31be5389b626010ee0917e1bc29e17e
Neil Zhao <zzrcxb@gmail.com>
2017-10-11 02:01:17 +0000

    fix a stupid error

--- src/symbolic_jump/jmp_table.c
+++ src/symbolic_jump/jmp_table.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
-#include "utils.h"
-
 #define jmp(addr) asm("jmp *%0"::"r"(addr):)
 
 #include "a_tester.h"
+#include "utils.h"
 
 int sym_checker(int i) {
     int addrs[] = {0,&&flag_1-&&flag_0,&&flag_2-&&flag_0,&&flag_3-&&flag_0,&&flag_4-&&flag_0,&&flag_5-&&flag_0};
     //printf("%x,%x,%x,%x,%x,%x, %d\n", &&flag_0, &&flag_1, &&flag_2, &&flag_3, &&flag_4, &&flag_5, addr);
-    jmp(&&flag_0 + addrs[atoi(i+1]);
+    jmp(&&flag_0 + addrs[i+1]);
     flag_0:
         return BOMB_ENDING;
     flag_1:
