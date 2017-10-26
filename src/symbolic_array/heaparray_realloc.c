7f6663a8a2eb7c7c8e2c627c88558238a89ef962
hxuhack <china.xuhui@gmail.com>
2017-10-26 22:46:44 +0800

    add a new case: heaparray_realloc.c

---
+++ src/symbolic_array/heaparray_realloc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"utils.h"
+#include "a_tester.h"
+
+int logic_bomb(int i) {
+    int *array = (int *) malloc(sizeof(int) * 5);
+    int k = 0;
+    for (k=0; k<5; k++){
+        array[k] = k;
+    }
+    array = (int *) realloc (array, sizeof(int) * 10);
+    for (k=5; k<10; k++){
+        array[k] = k;
+    }
+    if(array[i] == 7){
+        return BOMB_ENDING;
+     }
+    else
+        return NORMAL_ENDING;
+}
+
