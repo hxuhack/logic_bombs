512187292d5660cb7629a79bef3f16589aa03932
hxuhack <china.xuhui@gmail.com>
2017-10-27 12:03:41 +0800

    fix ping_csv

--- src/contextual_symbolic_value/ping_csv.c
+++ src/contextual_symbolic_value/ping_csv.c
@@ -27,7 +27,7 @@ int ping_it(struct in_addr *dst)
     int sock = socket(AF_INET,SOCK_DGRAM,IPPROTO_ICMP);
     if (sock < 0) {
         perror("socket");
-        return 0;
+        return -1;
     }
 
     memset(&addr, 0, sizeof addr);
@@ -38,56 +38,50 @@ int ping_it(struct in_addr *dst)
     icmp_hdr.type = ICMP_ECHO;
     icmp_hdr.un.echo.id = 1234;//arbitrary id
 
-    int count = 0;
-    for(; count < 4; count ++) {
-        unsigned char data[2048];
-        int rc;
-        struct timeval timeout = {3, 0}; //wait max 3 seconds for a reply
-        fd_set read_set;
-        socklen_t slen;
-        struct icmphdr rcv_hdr;
+    unsigned char data[2048];
+    int rc;
+    struct timeval timeout = {1, 0}; //wait max 3 seconds for a reply
+    fd_set read_set;
+    socklen_t slen;
+    struct icmphdr rcv_hdr;
 
-        icmp_hdr.un.echo.sequence = sequence++;
-        memcpy(data, &icmp_hdr, sizeof icmp_hdr);
-        memcpy(data + sizeof icmp_hdr, "hello", 5); //icmp payload
-        rc = sendto(sock, data, sizeof icmp_hdr + 5,
-                        0, (struct sockaddr*)&addr, sizeof addr);
-        if (rc <= 0) {
-            perror("Sendto");
-            break;
-        }
-        puts("Sent ICMP\n");
+    icmp_hdr.un.echo.sequence = sequence++;
+    memcpy(data, &icmp_hdr, sizeof icmp_hdr);
+    memcpy(data + sizeof icmp_hdr, "hello", 5); //icmp payload
+    rc = sendto(sock, data, sizeof icmp_hdr + 5,
+                    0, (struct sockaddr*)&addr, sizeof addr);
+    if (rc <= 0) {
+        perror("Sendto");
+    }
+    puts("Sent ICMP\n");
 
-        memset(&read_set, 0, sizeof read_set);
-        FD_SET(sock, &read_set);
+    memset(&read_set, 0, sizeof read_set);
+    FD_SET(sock, &read_set);
 
-        //wait for a reply with a timeout
-        rc = select(sock + 1, &read_set, NULL, NULL, &timeout);
-        if (rc == 0) {
-            puts("Got no reply\n");
-            continue;
-        } else if (rc < 0) {
-            perror("Select");
-            break;
-        }
+    //wait for a reply with a timeout
+    rc = select(sock + 1, &read_set, NULL, NULL, &timeout);
+    if (rc == 0) {
+        puts("Got no reply\n");
+    	return 0;
+    } else if (rc < 0) {
+        perror("Select");
+    	return 0;
+    }
 
-        //we don't care about the sender address in this example..
-        slen = 0;
-        rc = recvfrom(sock, data, sizeof data, 0, NULL, &slen);
-        if (rc <= 0) {
-            perror("recvfrom");
-            break;
-        } else if (rc < sizeof rcv_hdr) {
-            printf("Error, got short ICMP packet, %d bytes\n", rc);
-            break;
-        }
-        memcpy(&rcv_hdr, data, sizeof rcv_hdr);
-        if (rcv_hdr.type == ICMP_ECHOREPLY) {
-            printf("ICMP Reply, id=0x%x, sequence =  0x%x\n",
-                            icmp_hdr.un.echo.id, icmp_hdr.un.echo.sequence);
-        } else {
-            printf("Got ICMP packet with type 0x%x ?!?\n", rcv_hdr.type);
-        }
+    //we don't care about the sender address in this example..
+    slen = 0;
+    rc = recvfrom(sock, data, sizeof data, 0, NULL, &slen);
+    if (rc <= 0) {
+        perror("recvfrom");
+    } else if (rc < sizeof rcv_hdr) {
+        printf("Error, got short ICMP packet, %d bytes\n", rc);
+    }
+    memcpy(&rcv_hdr, data, sizeof rcv_hdr);
+    if (rcv_hdr.type == ICMP_ECHOREPLY) {
+        printf("ICMP Reply, id=0x%x, sequence =  0x%x\n",
+                        icmp_hdr.un.echo.id, icmp_hdr.un.echo.sequence);
+    } else {
+        printf("Got ICMP packet with type 0x%x ?!?\n", rcv_hdr.type);
     }
     return 1;
 }
@@ -100,13 +94,12 @@ int logic_bomb(char* s) {
     if (inet_aton(s, &dst) == 0) {
         perror("inet_aton");
         printf("%s isn't a valid IP address\n", s);
-        return 1;
+        return NORMAL_ENDING;
     }
 
-    if (ping_it(&dst)){
+    if (ping_it(&dst) == 1){
         return BOMB_ENDING;
     }else{
         return NORMAL_ENDING;
     }
-    return 0;
 }
