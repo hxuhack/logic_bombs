c6c6e604d21cfd27d132bef3026c2583807fdb1a
hxuhack <china.xuhui@gmail.com>
2017-10-28 23:40:10 +0800

    add a float case

---
+++ src/floating_point/float4_fp_l2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "a_tester.h"
+
+int logic_bomb(float symvar) {
+    printf("symvar = %f\n", symvar);
+    float x = symvar/-10000.0;
+    printf("x = %f\n", x);
+    if(1024+x == 1024 && x>0)
+        return BOMB_ENDING;
+    else
+        return NORMAL_ENDING;
+}
