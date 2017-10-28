711d7f9b3f231e0859ef10483160191daecb3ebf
hxuhack <china.xuhui@gmail.com>
2017-10-28 20:41:07 +0800

    add new case df2cf_cp

---
+++ src/covert_propogation/df2cf_cp.c
@@ -0,0 +1,58 @@
+/*
+TOY:
+*/
+#include <string.h> 
+#include "utils.h"
+
+#include "a_tester.h"
+
+int df2cf(char a)
+{
+    int b;
+    switch(a){
+      case 0:
+        b = 0;
+	break;
+      case 1:
+        b = 1;
+	break;
+      case 2:
+        b = 2;
+	break;
+      case 3:
+        b = 3;
+	break;
+      case 4:
+        b = 4;
+	break;
+      case 5:
+        b = 5;
+	break;
+      case 6:
+        b = 6;
+	break;
+      case 7:
+        b = 7;
+	break;
+      case 8:
+        b = 8;
+	break;
+      case 9:
+        b = 9;
+	break;
+      default:
+        b = 0;
+        break;
+    }
+    return b;
+}
+
+int logic_bomb(int symvar) {
+   int a = df2cf(symvar%10);
+   a++;
+   int b = symvar + a;
+   if(b == 15)
+    return BOMB_ENDING;
+   else
+    return NORMAL_ENDING;
+}
